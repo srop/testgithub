5d3178f4a195c81fdddd4bd0d3440a76f7182fe6
srop <por.srop@gmail.com>
2018-04-15 16:24:09 +0700

    test add font

---
+++ index.php
@@ -0,0 +1,128 @@
+<html>
+<head>
+<title>MCOT DTV ระบบเครื่องส่งทีวีดิจิตอล</title>
+
+<style type="text/css">
+@font-face {
+    font-family: 'ThaiSansLite';
+    src: url('font/ThaiSansLite.eot?#iefix') format('embedded-opentype'),
+           url('font/ThaiSansLite.woff') format('woff'),
+           url('font/ThaiSansLite.ttf') format('truetype');
+		   url('ThaiSansLite.svg#ThaiSansLite') format('svg');
+    font-weight: normal;
+    font-style: normal;
+}
+
+.font_article {
+    font-family: ThaiSansLite;
+    font-size: 25px;
+}
+</style>
+
+
+
+
+
+
+<style type="text/css">
+
+body {
+        margin:0;
+        padding:0;
+        font: bold 20px/5em;
+		font-family: 'ThaiSansLite';
+}
+
+h2 {
+        font: bold 30px ThaiSansLite, Verdana, Arial, Helvetica, sans-serif;
+        color: #000;
+        margin: 0px;
+        padding: 0px 0px 0px 15px;
+}
+
+img {
+border: none;
+}
+
+/*- Menu Tabs 5--------------------------- */
+
+    #tabs5 {
+      float:left;
+      width:100%;
+      background:#F4F4F4;
+      font-size:150%;
+	  font: bold;
+      line-height:normal;
+
+      }
+    #tabs5 ul {
+          margin:0;
+          padding:10px 10px 0 5px;
+          list-style:none;
+      }
+    #tabs5 li {
+      display:inline;
+      margin:0;
+      padding:0;
+      }
+    #tabs5 a {
+      float:left;
+      background:url("tableft5.gif") no-repeat left top;
+      margin:0;
+      padding:0 0 0 4px;
+      text-decoration:none;
+      }
+    #tabs5 a span {
+      float:left;
+      display:block;
+      background:url("tabright5.gif") no-repeat right top;
+      padding:1px 15px 4px 10px;
+      color:#FFF;
+      }
+    /* Commented Backslash Hack hides rule from IE5-Mac \*/
+    #tabs5 a span {float:none;}
+    /* End IE5-Mac hack */
+    #tabs5 a:hover span {
+      color:#FFF;
+      }
+    #tabs5 a:hover {
+      background-position:0% -42px;
+      }
+    #tabs5 a:hover span {
+      background-position:100% -42px;
+      }
+
+      #tabs5 #current a {
+      background-position:0% -42px;
+      }
+      #tabs5 #current a span {
+       background-position:100% -42px;
+      }
+
+</style>
+</head>
+
+<body>
+<div id="tabs5">
+<br>
+<h2>&nbsp;DTV MCOT WEB</h2>
+<ul>
+<!-- CSS Tabs -->
+<li id="current"><a href="home.html"><span>หน้าแรก</span></a></li>
+<li><a href="products.html"><span>บทความรู้</span></a></li>
+<li><a href="services.html"><span>หลักสูตร</span></a></li>
+<li><a href="support.html"><span>กิจกรรม</span></a></li>
+<li><a href="news.html"><span>ตารางเรียน</span></a></li>
+<li><a href="about.html"><span>เกี่ยวกับเรา</span></a></li>
+<li><a href="order.html"><span>ติดต่อเรา</span></a></li>
+</ul>
+</div>
+<font class="font_article" > 
+&nbsp;ศูนย์การเรียนรู้เทคโนโลยีดิจิตอล ระบบเครื่องส่งโทรทัศน์ และวิศวกรรม
+<BR>&nbsp;บทความด้านเชิงช่าง งานคอมพิวเตอร์ และการควบคุมเครื่องส่งโทรทัศน์
+<BR>&nbsp;แบ่งปันความรู้ให้กับบุคคลต่างๆที่มีความสนใจศึกษาเรื่องงานช่างวิศวกรรม
+<BR>&nbsp;เพื่อนำไปต่อยอดในการออกแบบเทคโนโลยีดิจิตอลและงานวิศวกรรม
+</font>
+
+</body>
+</html>
